829aaa19a3f56983b229c6b43ed6163b4f407c7d
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-10-01 10:04:32 +0000

    error fixing

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-from flask import Flask, render_template,request
+"""My first website"""
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ["GET"])
+"""This is for get method"""
+
+
+@app.route('/', methods=["GET"])
 def login():
+    """for login page"""
     return render_template("index.html")
 
-@app.route('/LogIn', methods = ["POST"])
+
+@app.route('/LogIn', methods=["POST"])
 def submit():
+    """for submit page"""
     name = request.form.get('Email')
     passw = request.form.get('Password')
     if name == passw:
         return f"your Email is {name}, and your password is {passw}"
     else:
         return render_template("index1.html")
+
 
 if __name__ == "__main__":
     app.run(port=5001)
